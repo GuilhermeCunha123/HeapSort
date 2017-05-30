ee17889ce40ec992e95e1eea9e68306dd51b06c8
Guilherme Barbosa <nguilhermebarbosa@gmail.com>
2017-05-30 20:34:59 -0300

    Adicionada a persistência do Usuario

--- App_Code/Persistencia/usu_usuarioDB.cs
+++ App_Code/Persistencia/usu_usuarioDB.cs
@@ -2,11 +2,103 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
 
 /// <summary>
 /// Descrição resumida de Class1
 /// </summary>
 public class Class1
 {
-    
+    public static int Insert(usu_usuario usu)
+    {
+        int retorno = 0;
+
+        try
+        {
+            //Correto
+            IDbConnection objConexao; //Abrir a conexão
+            IDbCommand objCommand; // Criar e executar os comandos
+            string sql = "insert into usu_usuario ";
+            sql += "(usu_email, usu_senha) ";
+            sql += "values ";
+            sql += "(?usu_email, ?usu_senha) ";
+
+            objConexao = Mapped.Connection();
+            objCommand = Mapped.Command(sql, objConexao);
+
+
+            objCommand.Parameters.Add(Mapped.Parameter("?usu_email", usu.Usu_email));
+            objCommand.Parameters.Add(Mapped.Parameter("?emp_senha", usu.Usu_senha));
+            
+            /*
+            // Chave estrangeira
+            objCommand.Parameters.Add(Mapped.Parameter("?tip_id", emp.Tip_id.Tip_id));
+            */
+
+            objCommand.ExecuteNonQuery();
+            objConexao.Close();
+            objConexao.Dispose();
+            objCommand.Dispose();
+        }
+        catch (Exception)
+        {
+            //erro
+            retorno = -2;
+        }
+        return retorno;
+    }
+
+    public static DataSet SelectAll()
+    {
+        DataSet ds = new DataSet();
+        IDbConnection objConexao;
+        IDbCommand objCommand;
+
+        IDataAdapter objDataAdapter;
+        //string sql = "select emp_nome as NOME, emp_rua as RUA from emp_empresa order by emp_nome";
+        //string sql = "select emp_nome, emp_rua from emp_empresa order by emp_nome";
+        string sql = "select * from usu_usuario";
+
+        objConexao = Mapped.Connection();
+        objCommand = Mapped.Command(sql, objConexao);
+
+        objDataAdapter = Mapped.Adapter(objCommand);
+        objDataAdapter.Fill(ds);
+
+        objConexao.Close();
+        objConexao.Dispose();
+        objCommand.Dispose();
+
+        return ds;
+    }
+
+    public static int Delete(int id)
+    {
+        int retorno = 0;
+
+        try
+        {
+            //Correto
+            IDbConnection objConexao; //Abrir a conexão
+            IDbCommand objCommand; // Criar e executar os comandos
+            string sql = "delete from usu_usuario where usu_id = ?usu_id";
+
+            objConexao = Mapped.Connection();
+            objCommand = Mapped.Command(sql, objConexao);
+
+
+            objCommand.Parameters.Add(Mapped.Parameter("?usu_id", id));
+
+            objCommand.ExecuteNonQuery();
+            objConexao.Close();
+            objConexao.Dispose();
+            objCommand.Dispose();
+        }
+        catch (Exception)
+        {
+            //erro
+            retorno = -2;
+        }
+        return retorno;
+    }
 }
