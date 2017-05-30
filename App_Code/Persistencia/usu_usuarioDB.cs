04e89c92243434a495cbd80419135f52e1bc31f2
GuilhermeCunha123 <guiaraujocunha@gmail.com>
2017-05-30 20:10:36 -0300

    dhdfh

---
+++ App_Code/Persistencia/usu_usuarioDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de Class1
+/// </summary>
+public class Class1
+{
+    
+}
